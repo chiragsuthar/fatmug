6820129f8039101ad0ed4c3175e89c1e079fb849
Chirag Suthar <47880785+chiragsuthar@users.noreply.github.com>
2023-12-14 03:02:42 +0530

    Update admin.py
    add admin.py file

--- vendor_management_system/vendor/admin.py
+++ vendor_management_system/vendor/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
 
 # Register your models here.
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'contact_details', 'address', 'vendor_code', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate']
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ['po_number', 'vendor', 'order_date', 'delivery_date', 'items', 'quantity', 'status', 'quality_rating', 'issue_date', 'acknowledgment_date']
+
+class HistoricalPerformanceAdmin(admin.ModelAdmin):
+    list_display = ['vendor', 'date', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate']
+
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
+admin.site.register(HistoricalPerformance, HistoricalPerformanceAdmin)
